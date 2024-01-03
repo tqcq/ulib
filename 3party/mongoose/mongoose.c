af26f99991e569b39baacefcf05a533e4b8b1dde
tqcq <99722391+tqcq@users.noreply.github.com>
2024-01-03 17:33:38 +0800

    fix build failed.

--- 3party/mongoose/mongoose.c
+++ 3party/mongoose/mongoose.c
@@ -17,7 +17,12 @@
 //
 // SPDX-License-Identifier: GPL-2.0-only or commercial
 
+// fix miss CLOCK_REALTIME
+#define _POSIX_C_SOURCE 200809L
 #include "mongoose.h"
+#include <time.h>
+#include <alloca.h>
+#include <unistd.h>
 
 #ifdef MG_ENABLE_LINES
 #line 1 "src/base64.c"
